9fc8d985cb44f7caf043d8764a599b55a9a5699e
Daniel Werner <99842410+DanielWerner07@users.noreply.github.com>
2022-03-10 18:30:55 -0600

    done

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ window.addEventListener("load", function() {
       response.json().then(function(json) {
         const div = document.getElementById("container")
         for (let i = 0; i < json.length; i++) {
-            div.innerHTML += `
+            if (json[i].active === true) {
+                div.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                            <li class="active">Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src=${json[i].picture}>
+                    </div>
+            `;
+            } else {
+                div.innerHTML += `
                 <div class="astronaut">
                     <div class="bio">
                         <h3>${json[i].firstName} ${json[i].lastName}</h3>
@@ -16,6 +31,7 @@ window.addEventListener("load", function() {
                     <img class="avatar" src=${json[i].picture}>
                     </div>
             `;
+            }
         };
         div.innerHTML += `<h1>total astronaut count: ${json.length}</h1>`
       });
